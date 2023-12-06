47754b9a84b50fd0bf176c020020771cbd0dec4b
Komo4ek <laivhakin@gmail.com>
2023-12-06 15:49:32 +0100

    class refactor. new data

--- src/RighiDiary/_user.py
+++ src/RighiDiary/_user.py
@@ -1,5 +1,8 @@
-from src.RighiDiary.baseprofile import BaseProfile
-from typing import Union
+from typing import Union, List
+import asyncio
+
+from ._baseprofile import BaseProfile
+from ._agenda import Agenda, get_user_agenda
 
 
 class User(BaseProfile):
@@ -10,16 +13,24 @@ class User(BaseProfile):
         self,
         login: int,
         password: str,
-        name: str,
-        surname: str,
-        mastercom_id: int,
-        current_key: str,
+        name: Union[str, None],
+        surname: Union[str, None],
+        mastercom_id: Union[str, None],
+        current_key: Union[str, None],
+        classes: str,
+        phone: Union[str, None],
+        email: Union[str, None],
+        agenda: Union[List[Agenda], None],
     ):
         super().__init__(login=login, password=password)
-        self._current_key = current_key
-        self._mastercom_id = mastercom_id
+        self.current_key = current_key
+        self.mastercom_id = mastercom_id
         self._surname = surname
         self._name = name
+        self.classes = classes
+        self.phone = phone
+        self.email = email
+        self.agenda = agenda
 
     @property
     def name(self) -> Union[str, None]:
@@ -42,7 +53,13 @@ class User(BaseProfile):
         name = self.name
         surname = self.surname
 
-        full_name = f"{surname + ' ' if surname is not None else ''}{name if name is not None else ''}"
+        full_name = f"{(surname + ' ') if surname is not None else ''}{name if name is not None else ''}"
 
         return full_name if full_name else None
+
+    async def update_user_agenda(self) -> List[Agenda]:
+        new_agenda = await get_user_agenda(login=super().login, password=super().password)
+        self.agenda = new_agenda
+
+        return new_agenda
 
