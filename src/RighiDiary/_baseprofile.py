6f3ec178bc44e099cc8463e50ed82e9895cb86bc
Komo4ek <laivhakin@gmail.com>
2023-12-06 16:31:56 +0100

    delete some code

--- src/RighiDiary/_baseprofile.py
+++ src/RighiDiary/_baseprofile.py
@@ -1,12 +1,4 @@
 class BaseProfile:
     def __init__(self, login: int, password: str):
-        self._login = login
-        self._password = password
-
-    @property
-    def login(self) -> int:
-        return self._login
-
-    @property
-    def password(self) -> str:
-        return self._password
+        self.login = login
+        self.password = password
