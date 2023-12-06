f647db53d816612ae2090ab204f877f79d576a3d
Komo4ek <laivhakin@gmail.com>
2023-12-06 15:48:08 +0100

    imports fix

--- src/RighiDiary/__init__.py
+++ src/RighiDiary/__init__.py
@@ -1,4 +1,11 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __logger__ = "RighiDiary"
 
-from user import User
+from ._user import User
+from ._auth_functions import (
+    AuthData,
+    fast_auth,
+    get_current_key,
+)
+from ._authorize_user import authorize_user
+from ._agenda import Agenda, get_user_agenda
